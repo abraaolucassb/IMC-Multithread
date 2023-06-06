d6807aefaf9ed361ee61150f45e7b03a86d0d75b
Abraão Lucas <abraaolucassb@gmail.com>
2023-06-06 09:53:26 -0300

    server documentation

--- server.py
+++ server.py
@@ -86,6 +86,16 @@ def processingDataClient(received):
     received['tmb'] = generateTMB(received)
 
     def generateCal(dict):
+        '''Generate cal based on user data.
+
+        Args:
+        dict (dict): 
+            - nvlAtiv (int): Level of physical activity.
+            - tmb (float): Bala Metabolic Rate.
+
+        Returns:
+            float: Cal value.
+        '''
         if dict['nvlAtiv'] == 1:
             fatorAtiv = 1.2
 
@@ -104,6 +114,17 @@ def processingDataClient(received):
     received['cal'] = generateCal(received)
 
     def generateNutrients(dict):
+        """Generate Nutrients from cal value.
+
+        Args: 
+            dict (dict): Dict containing cal value.
+
+        Returns:
+            dict: Dict containing nutrients values.
+                - 'carboidratos' (str): Value of carbohydrates.
+                - 'proteinas' (str): Value of proteins.
+                - 'gorduras' (str): Value of fats.
+        """
         carb = str(round((dict['cal'] * 0.45), 2))
         prot = str(round((dict['cal'] * 0.3), 2))
         fat = str(round((dict['cal'] * 0.25), 2))
@@ -116,6 +137,16 @@ def processingDataClient(received):
 
 
 def handleClient(clientSocket, addr):
+    '''Makes connection with the client and receives their data.
+
+    Args: 
+        clientSocket (socket): The socket object of the connection to the client.
+        addr (tuple): The tuple containing the IP address and port of the client.
+
+
+    Returns:
+        str: Sends to the client the result of processing the received data in JSON format.
+    '''
     print('Conectado a {}'.format(str(addr)))
 
     # Recive client data.
